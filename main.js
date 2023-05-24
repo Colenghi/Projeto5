4c2d622b7a6d5e57c9df69965ea642f7bb04dee5
MacBook Air M1 <macbookairm1@MacBook-Air-de-Felipe-Colenghi.local>
2023-05-24 14:59:15 -0300

    p5js

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload(){}
+
+function setup()
+{
+    canvas = createCanvas(640, 480);
+    canvas.position(150, 150);
+    video = createCapture(VIDEO)
+    video.hide();
+}
+
+function draw()
+{
+    image(video, 230, 150, 220, 200);
+    fill(255, 118, 10)
+    stroke(255, 0, 0);
+    circle(50, 50, 80);
+    circle(50, 430, 80);
+    circle(590, 50, 80);
+    circle(335, 430, 80);
+    circle(590, 430, 80);
+    circle(335,50, 80);
+    fill(140, 128, 0);
+    stroke(0, 128, 0);
+    rect(90, 40, 205, 20);
+    rect(90, 420, 205, 20);
+    rect(40, 90, 20, 300);
+    rect(375, 40, 175, 20);
+    rect(375, 420, 175, 20);
+    rect(580, 90, 20, 300);
+}
+
+function take_snapshot(){    
+    save('student_name.png');
+  }
